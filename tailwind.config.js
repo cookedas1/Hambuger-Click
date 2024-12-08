2a63bb4c53b09e9676e3d0c5c9cb7d45067e54d8
cookedas1 <mulmando1991@gmail.com>
2024-12-08 12:08:25 +0900

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/**/*.html', // HTML 파일 경로
+  ],
+  theme: {
+    extend: {}, // 사용자 정의 스타일 확장 가능
+  },
+  plugins: [],
+};
